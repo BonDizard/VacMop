57e14230ffce09f646a66549b45faa2b4a6a7d87
Bharath Kumar s <114819425+BonDizard@users.noreply.github.com>
2023-03-13 09:48:41 +0530

    Update Combined.cc

--- Combined.cc
+++ Combined.cc
@@ -11,6 +11,7 @@ int echo = 9;
 int time = 0;
 int t1;
 int pos;
+int x;
 
 void setup() {
 
@@ -23,87 +24,104 @@ void setup() {
   pinMode(3, INPUT); //RIGHT IR
   pinMode(trig, OUTPUT); //ULTRA SONIC
   pinMode(echo, INPUT);
-}
+turnFront();
+      }
 
 void loop() {
   if (Serial.available()) {
-    digitalWrite(13, LOW);
-    digitalWrite(11, LOW);
-    digitalWrite(10, LOW);
-    digitalWrite(12, LOW);
+    while(true){
     t = Serial.read();
     Serial.println(t);
-    if (t == 'F') { //Forward
+    if (t == 'F') {
       digitalWrite(13, HIGH);
       digitalWrite(11, HIGH);
-    } else if (t == 'R') { //Reverse
+    } else if (t == 'B') {
       digitalWrite(12, HIGH);
       digitalWrite(10, HIGH);
-    } else if (t == 'L') { //Left
+    } else if (t == 'L') {
       digitalWrite(11, HIGH);
-    } else if (t == 'D') { //Right
+    } else if (t == 'R') {
       digitalWrite(13, HIGH);
-    } else if (t == 'G') {
-      while (true) {
-        digitalWrite(trig, HIGH);
-        delayMicroseconds(10);
-        digitalWrite(trig, LOW);
-        t1 = pulseIn(echo, HIGH);
-        distance = (t1 * 0.034) / 2;
-        Serial.println(distance);
-
-        left_ir = digitalRead(2);
-        right_ir = digitalRead(3);
-
-        if ((distance >= 5) && (distance <= 15) && (left_ir == LOW) && (right_ir == LOW)) {
-
-          digitalWrite(13, HIGH);
-          digitalWrite(11, HIGH);
-          delay(500);
-          digitalWrite(13, LOW);
-          digitalWrite(11, LOW);
-          digitalWrite(10, LOW);
-          digitalWrite(12, LOW);
-        } else if (left_ir == HIGH && (distance >= 5) && (distance <= 15)) {
-          digitalWrite(13, HIGH);
-          delay(500);
-          digitalWrite(13, LOW);
-          digitalWrite(11, LOW);
-          digitalWrite(10, LOW);
-          digitalWrite(12, LOW);
-        } else if (right_ir == HIGH && (distance >= 5) && (distance <= 15)) {
-          digitalWrite(11, HIGH);
-          delay(500);
-          digitalWrite(13, LOW);
-          digitalWrite(11, LOW);
-          digitalWrite(10, LOW);
-          digitalWrite(12, LOW);
-        } else if ((distance > 0) && (distance < 5)) {
-          digitalWrite(12, HIGH);
-          digitalWrite(10, HIGH);
-          delay(500);
-          digitalWrite(13, LOW);
-          digitalWrite(11, LOW);
-          digitalWrite(10, LOW);
-          digitalWrite(12, LOW);
-        } else {
-          digitalWrite(13, LOW);
-          digitalWrite(11, LOW);
-          digitalWrite(10, LOW);
-          digitalWrite(12, LOW);
-        }
-
-        t = Serial.read();
-        Serial.println(t);
-        if (t == 'E') {
-          break;
-        }
+    } else if (t == 'S') {
+      digitalWrite(13, LOW);
+      digitalWrite(12, LOW);
+      digitalWrite(11, LOW);
+      digitalWrite(10, LOW);
+    } 
+    else if(t=='U'){
+      break;
+    }
+    else if (t == 'W') { //Gesture
+      distance = getDistance();
+      left_ir = digitalRead(2);
+      right_ir = digitalRead(3);
+      if ((distance >= 5) && (distance <= 15) && (left_ir == LOW) && (right_ir == LOW)) {
+        forward(1000);
+        delay(500);
+        stopWheels();
+      } else if (left_ir == HIGH && (distance >= 5) && (distance <= 15)) {
+        right(1000);
+        delay(500);
+        stopWheels();
+      } else if (right_ir == HIGH && (distance >= 5) && (distance <= 15)) {
+        left(1000);
+        delay(500);
+        stopWheels();
+      } else if ((distance > 0) && (distance < 5)) {
+        reverse(1000);
+        delay(500);
+        stopWheels();
+      } else {
+       stopWheels(); 
       }
     }
+    }
+  }  else if (!Serial.available()){
+    autoPi();
   }
-
+}
+ void turnFront(){
+        myservo.attach(7);
+      for (pos = 0; pos >= 90; pos += 1) {
+        myservo.write(pos);
+        delay(15);
+      }
+  }
+void stopWheels() {
+  digitalWrite(13, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(12, LOW);
+}
+void forward(int x) {
+  digitalWrite(13, HIGH);
+  digitalWrite(12, LOW);
+  digitalWrite(11, HIGH);
+  digitalWrite(10, LOW);
+  delay(x);
+}
+void reverse(int x) {
+  digitalWrite(10, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(11, LOW);
+  digitalWrite(13, LOW);
+  delay(x);
 }
 
+void left(int x) {
+  digitalWrite(11, HIGH);
+  digitalWrite(13, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(10, LOW);
+  delay(2000);
+}
+void right(int x) {
+  digitalWrite(13, HIGH);
+  digitalWrite(12, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);
+  delay(x);
+}
 void left_u() {
   digitalWrite(11, HIGH);
   delay(5000);
@@ -126,49 +144,50 @@ int getDistance() {
 }
 
 void autoPi() {
+  for (pos = 0; pos >= 90; pos += 1) {
+        myservo.write(pos);
+        delay(15);
+      }
   distance = getDistance();
-  left_ir = digitalRead(2);
-  right_ir = digitalRead(3);
-
   if (distance <= 15) {
     myservo.attach(7);
-    digitalWrite(13, LOW);
-    digitalWrite(11, LOW);
-    digitalWrite(10, LOW);
-    digitalWrite(12, LOW);
-    delay(2000);
+    stopWheels();
+    delay(1000);
     while (true) {
+      for (pos = 90; pos >= 0; pos -= 1) {
+        myservo.write(pos);
+        delay(15);
+      }
+      delay(2000);
+      distance = getDistance();
+      if (!(distance <= 15)) {
+        left_u();
       for (pos = 0; pos <= 90; pos += 1) {
         myservo.write(pos);
         delay(15);
       }
-      distance = getDistance();
-
-      if (!(distance <= 15)) {
-        delay(2000);
-        left_u();
         break;
-      }
-      delay(5000);
-      for (pos = 90; pos <= 180; pos += 1) {
-        myservo.write(pos);
-        delay(1000);
-      }
-      distance = getDistance();
-      if (!(distance <= 15)) {
-        delay(2000);
-        right_u();
-        break;
-        delay(5000);
-      } else {
-        digitalWrite(13, LOW);
-        digitalWrite(11, LOW);
-        digitalWrite(10, LOW);
-        digitalWrite(12, LOW);
+      } 
+      else {
+        for (pos = 90; pos <= 180; pos += 1) {
+          myservo.write(pos);
+          delay(15);
+        }
+                delay(2000);
+        distance = getDistance();
+        if (!(distance <= 15)) {
+          right_u();
+          for (pos = 180; pos >= 90; pos -= 1) {
+          myservo.write(pos);
+          delay(15);
+        }
+          break;
+        } else {
+          stopWheels();
+        }
       }
     }
   } else {
-    digitalWrite(13, HIGH);
-    digitalWrite(11, HIGH);
+    forward(1000);
   }
 }
